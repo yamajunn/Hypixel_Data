068ce7c6b9275379106dc12f563e0ef4a05159ba
yamajunn <amiton1115@gmail.com>
2023-12-12 18:04:06 +0900

    win

--- Code/BedWarsData.py
+++ Code/BedWarsData.py
@@ -1,11 +1,11 @@
 from BedWarsStatus import bedwars_status
 from Who import who
-import sqlite3
 import csv
+import datetime
 
-path = r"/Users/chinq500/Library/Application Support/minecraft/versions/1.8.9/logs/latest.log"
-# path = r"C:/Users/Owner/AppData/Roaming/.minecraft/logs/blclient/minecraft/latest.log"
-write_list = [i for i in range(41)]
+# path = r"/Users/chinq500/Library/Application Support/minecraft/versions/1.8.9/logs/latest.log"
+path = r"C:/Users/Owner/AppData/Roaming/.minecraft/logs/blclient/minecraft/latest.log"
+write_list = [i for i in range(49)]
 
 with open(path) as f:
     s = f.read()
@@ -13,12 +13,19 @@ with open(path) as f:
 who_list = who(s)
 
 data_list = []
-for item in who_list:
-    data_list.append(bedwars_status(item))
-
-with open('./PlayData.csv', 'a') as f:
+# who_list = ["Gokiton"]
+dt_now = datetime.datetime.now()
+with open('./PlayerStatus.csv', 'a', newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(write_list)
+    for item in who_list:
+        data = bedwars_status(item)
+        if data[0] != True:
+            data.insert(0,dt_now)
+            writer.writerow(data)
+        else:
+            data = [dt_now, data[1]]
+            writer.writerow(data)
 
-with open('./PlayData.csv') as f:
-    print(f.read())
+# play_data_list = [dt_now, ]
+# with open('./PlayData.csv', 'a') as f:
+#     writer = csv.writer(f)
--- Code/BedWarsStatus.py
+++ Code/BedWarsStatus.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 def bedwars_status(name):
     API_KEY = "253c7c06-d1fb-49b5-99e4-3c74cbb874f5"
     try:
@@ -25,16 +26,28 @@ def bedwars_status(name):
                             'kills_bedwars', 
                             'deaths_bedwars', 
                             'beds_broken_bedwars' , 
-                            'beds_lost_bedwars'
+                            'beds_lost_bedwars',
+                            'iron_resources_collected_bedwars',
+                            'gold_resources_collected_bedwars',
+                            'diamond_resources_collected_bedwars',
+                            'emerald_resources_collected_bedwars',
+                            'winstreak'
                             ]
                 
+                data_list.append(name)
+                data_list.append(data_dic["player"]["_id"])
                 data_list.append(data_dic["player"]['achievements']['bedwars_level'])
                 for item in dic_list:
                     data_list.append(data_dic["player"]["stats"]["Bedwars"][item])
+                data_list.append(round(data_list[3]/data_list[4], 2))
+                data_list.append(round(data_list[5]/data_list[6], 2))
+                data_list.append(round(data_list[7]/data_list[8], 2))
+                data_list.append(round(data_list[9]/data_list[10], 2))
                 return data_list
             else:
-                return True
+                return [True, name]
         else:
-            return True
+            return [True, name]
     except KeyError:
-        return True
+        return [True, name]
+# bedwars_status("Gokiton")
